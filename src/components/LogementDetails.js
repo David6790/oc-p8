7332b6668f91d30dd051cee6fe41acd300f6c412
long bin <david.lb90@gmail.com>
2023-07-12 12:27:01 +0200

    page de propriété quasiment fini et stylisé. reste à faire les collapses

--- src/components/LogementDetails.js
+++ src/components/LogementDetails.js
@@ -3,9 +3,30 @@ import Ratings from "./Ratings";
 
 const LogementDetails = ({ logement }) => {
   return (
-    <div>
-      <h1>{logement.title}</h1>
-      <Ratings logement={logement} />
+    <div className="cardDetailContainer">
+      <div className="cardTitleTags">
+        <h1>{logement.title}</h1>
+        <span>{logement.location}</span>
+        <div className="tags">
+          {logement.tags &&
+            logement.tags.map((item) => (
+              <div className="tagCard" key={crypto.randomUUID()}>
+                {item}
+              </div>
+            ))}
+        </div>
+      </div>
+      <div className="ownerRatings">
+        <div className="owner">
+          <div className="ownerName">{logement.host.name}</div>
+          <div className="ownerPicture">
+            <img src={logement.host.picture} alt="" />
+          </div>
+        </div>
+        <div className="ratings">
+          <Ratings logement={logement} />
+        </div>
+      </div>
     </div>
   );
 };
